f2fb7877afb4619fd64dc83caf1415cc412b8984
Odair Martins <omartins@fitesa.com>
2020-10-05 22:02:17 -0300

    third commit

--- js/install.js
+++ js/install.js
@@ -1,76 +1,38 @@
-/*
- * @license
- * Your First PWA Codelab (https://g.co/codelabs/pwa)
- * Copyright 2019 Google Inc. All rights reserved.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     https://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License
- */
-'use strict';
+let deferredPrompt; // Allows to show the install prompt
+let setupButton;
 
-let deferredInstallPrompt = null;
-const installButton = document.getElementById('add-buttom');
-installButton.addEventListener('click', installPWA);
+window.addEventListener('beforeinstallprompt', (e) => {
+    // Prevent Chrome 67 and earlier from automatically showing the prompt
+    e.preventDefault();
+    // Stash the event so it can be triggered later.
+    deferredPrompt = e;
+    console.log("beforeinstallprompt fired");
+    if (setupButton == undefined) {
+        setupButton = document.getElementById("setup_button");
+    }
+    // Show the setup button
+    setupButton.style.display = "inline";
+    setupButton.disabled = false;
+});
 
-// CODELAB: Add event listener for beforeinstallprompt event
-window.addEventListener('beforeinstallprompt', saveBeforeInstallPromptEvent);
-
-/**
- * Event handler for beforeinstallprompt event.
- *   Saves the event & shows install button.
- *
- * @param {Event} evt
- */
-function saveBeforeInstallPromptEvent(evt) {
-    // CODELAB: Add code to save event & show the install button.
-    deferredInstallPrompt = evt;
-    // installButton.removeAttribute('hidden');
-    document.getElementById('add-buttom').style.display = "block";
+function installApp() {
+  // Show the prompt
+  deferredPrompt.prompt();
+  setupButton.disabled = true;
+  // Wait for the user to respond to the prompt
+  deferredPrompt.userChoice
+      .then((choiceResult) => {
+          if (choiceResult.outcome === 'accepted') {
+              console.log('PWA setup accepted');
+              // hide our user interface that shows our A2HS button
+              setupButton.style.display = 'none';
+          } else {
+              console.log('PWA setup rejected');
+          }
+          deferredPrompt = null;
+      });
 }
 
-
-/**
- * Event handler for butInstall - Does the PWA installation.
- *
- * @param {Event} evt
- */
-function installPWA(evt) {
-    // CODELAB: Add code show install prompt & hide the install button.
-    deferredInstallPrompt.prompt();
-    // Hide the install button, it can't be called twice.
-    // evt.srcElement.setAttribute('hidden', true);
-    document.getElementById('add-buttom').style.display = 'none';
-    // CODELAB: Log user response to prompt.
-    deferredInstallPrompt.userChoice
-    .then((choice) => {
-      if (choice.outcome === 'accepted') {
-        console.log('User accepted the A2HS prompt', choice);
-      } else {
-        console.log('User dismissed the A2HS prompt', choice);
-      }
-      deferredInstallPrompt = null;
-    });
-}
-
-// CODELAB: Add event listener for appinstalled event
-window.addEventListener('appinstalled', logAppInstalled);
-
-/**
- * Event handler for appinstalled event.
- *   Log the installation to analytics or save the event somehow.
- *
- * @param {Event} evt
- */
-function logAppInstalled(evt) {
-    // CODELAB: Add code to log the event
-    console.log('IOP was installed.', evt);
-}
+window.addEventListener('appinstalled', (evt) => {
+  console.log("appinstalled fired", evt);
+});
